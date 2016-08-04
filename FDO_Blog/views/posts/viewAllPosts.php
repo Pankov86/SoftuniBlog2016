d1c4eb073a4045c56d0bee563d140d03597b1596
ognyanstanoev <ognyanstanoev@gmail.com>
2016-08-04 12:20:30 +0300

    Fixed posts content so that html tags have their meaning with "html_entity_decode"

--- FDO_Blog/views/posts/viewAllPosts.php
+++ FDO_Blog/views/posts/viewAllPosts.php
@@ -14,7 +14,7 @@
             <tr>
                 <td><?= htmlspecialchars($post['id'])?></td>
                 <td><a href="<?=APP_ROOT?>/home/view/<?=$post['id']?>"><?= htmlentities($post['title']); ?></a></td>
-                <td><?= cutLongText($post['content'])?></td>
+                <td><?= html_entity_decode($post['content'])?></td>
                 <td><?= htmlspecialchars($post['full_name'])?></td>
                 <td><?= htmlspecialchars($post['date'])?></td>
                 <td><?= htmlspecialchars($post['views_count'])?></td>
