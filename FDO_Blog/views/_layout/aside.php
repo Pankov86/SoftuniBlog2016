ca51d2a4f9e92c25e68343e9eba2b34b9c3e83fd
Martin <mrindzhov@gmail.com>
2016-08-30 23:38:16 +0300

    unnecessary doc

--- FDO_Blog/views/_layout/aside.php
+++
@@ -1,9 +0,0 @@
-<aside>
-    <h2>Categories</h2>
-    <a href="<?=APP_ROOT?>/home/">All</a>
-    <?php
-    $categories = HomeController::getAllCategories();
-    foreach ($categories as $category) : ?>
-        <a href="<?=APP_ROOT?>/home/category/<?=$category['id']?>"><?= htmlentities($category['category_name']); ?></a>
-    <?php endforeach; ?>
-</aside>
