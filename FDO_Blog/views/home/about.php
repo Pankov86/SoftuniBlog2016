7acae22d0b7c18051cbd6bc2d37b191a5bb50f3f
ognyanstanoev <ognyanstanoev@gmail.com>
2016-08-30 14:06:11 +0300

    Generated content for about page

--- FDO_Blog/views/home/about.php
+++ FDO_Blog/views/home/about.php
@@ -6,6 +6,25 @@
 </head>
 <body>
 <h1>About First Dollar online</h1>
-
+<p>This is a place where you can find the latest trends and opportunities when it comes to <strong>making money online</strong>. The authors and users of the blog have put relentless effort to bring you closer to your dreams and financial goals. So please, don't miss the opportunity to participate in this community and reap the benefits! </p>
+<h3>Here you can find:</h3>
+<ul>
+    <li>
+        Ecommerce advice
+    </li>
+    <li>
+        Social media marketing tips
+    </li>
+    <li>
+        Best practices of copywriting
+    </li>
+    <li>
+        SEO tips
+    </li>
+    <li>
+        How to find AND create opportunities for yourself as a freelancer
+    </li>
+</ul>
+<p>.....and much more!</p>
 </body>
 </html>
