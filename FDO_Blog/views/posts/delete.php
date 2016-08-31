a3d7a5dd662f08bbe96f69d11ed93bb0891dca4d
Martin <mrindzhov@gmail.com>
2016-08-31 18:19:05 +0300

    Update delete.php

--- FDO_Blog/views/posts/delete.php
+++ FDO_Blog/views/posts/delete.php
@@ -1,16 +1,33 @@
 <?php $this->title = 'Delete post'; ?>
 
-<h1><?=htmlspecialchars($this->title)?></h1>
 
 
+<form class="form-horizontal" method="post">
+    <fieldset>
+        <legend><h1><?=htmlspecialchars($this->title)?></h1>
+        </legend>
 
-<form method="post">
-    <div>Title:</div>
-    <input type="text" name="post_title" disabled
-           value="<?=htmlspecialchars($this->post['title'])?>">
-    <div>Content:</div>
-    <textarea rows="10" name="post_content" disabled
-    ><?=htmlspecialchars($this->post['content'])?></textarea>
-    <div><input type="submit" value="Delete post">
-        <a href="<?=APP_ROOT?>/posts">[Cancel]</a></div>
+
+        <div class="form-group">
+            <label for="inputEmail" class="col-lg-2 control-label">Title:</label>
+            <div class="col-lg-10">
+                <input type="text" class="form-control" id="inputEmail" name="post_title" value="<?=htmlspecialchars($this->post['title'])?>" disabled>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="textArea" class=" col-lg-2 control-label">Content</label>
+            <div class="col-lg-10">
+                <textarea class="form-control" rows="5" id="textArea" disabled><?=htmlspecialchars($this->post['content'])?></textarea>
+            </div>
+        </div>
+
+
+        <div class="form-group">
+            <div class="col-lg-10 col-lg-offset-2">
+                <button type="submit" class="btn btn-primary">Delete post</button>
+                <button type="reset" class="btn btn-default"><a href="<?=APP_ROOT?>/posts">Cancel</a></button>
+            </div>
+        </div>
+    </fieldset>
 </form>
