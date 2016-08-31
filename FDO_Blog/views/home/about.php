de2dd1a69e2e981be856253ab4e18aa72165a04c
Martin <mrindzhov@gmail.com>
2016-08-31 18:09:23 +0300

    Update about.php

--- FDO_Blog/views/home/about.php
+++ FDO_Blog/views/home/about.php
@@ -5,7 +5,9 @@
     <title>About</title>
 </head>
 <body>
-<h1>About First Dollar online</h1>
+<legend><h1>About First Dollar online</h1></legend>
+
+
 <p>This is a place where you can find the latest trends and opportunities when it comes to <strong>making money online</strong>. The authors and users of the blog have put relentless effort to bring you closer to your dreams and financial goals. So please, don't miss the opportunity to participate in this community and reap the benefits! </p>
 <h3>Here you can find:</h3>
 <ul>
