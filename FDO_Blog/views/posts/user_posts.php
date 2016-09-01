69f5497e09cf255069bb5d8df68390824c80b12c
Martin <mrindzhov@gmail.com>
2016-09-01 10:01:33 +0300

    Update user_posts.php

--- FDO_Blog/views/posts/user_posts.php
+++ FDO_Blog/views/posts/user_posts.php
@@ -1,27 +1,26 @@
 <main>
-    <table class="table table-stripped">
+    <table class="table table-striped">
         <tr>
             <th>Title</th>
             <th>Content</th>
             <th>Author</th>
             <th>Date</th>
-            <th>Actions</th>
-        </tr
-
-    <?php foreach ($this->posts as $post) : ?>
-        <tr>
-            <td><a href="<?=APP_ROOT?>/home/view/<?=$post['id']?>"><?= htmlentities($post['title']); ?></a></td>
-            <td><?= cutLongText($post['content'])?></td>
-            <td><?= htmlspecialchars($post['full_name'])?></td>
-            <td><?= htmlspecialchars($post['date'])?></td>        
-            
-            <td>
-                <a href="<?=APP_ROOT?>/posts/edit/<?=$post['id']?>">Edit</a>
-                <a href="<?=APP_ROOT?>/posts/delete/<?=$post['id']?>">Delete</a>
-            </td>
+            <th>Reads</th>
+            <th>Comments</th>
+            <th>Points</th>
         </tr>
 
-    <?php endforeach; ?>
+        <?php foreach ($this->posts as $post) : ?>
+            <tr>
+                <td><a href="<?=APP_ROOT?>/home/view/<?=$post['id']?>"><?= htmlentities($post['title']); ?></a></td>
+                <td><?= html_entity_decode($post['content'])?></td>
+                <td><?= htmlspecialchars($post['full_name'])?></td>
+                <td><?= htmlspecialchars($post['date'])?></td>
+                <td><?= htmlspecialchars($post['views_count'])?></td>
+                <td><?= htmlspecialchars($post['comments_count'])?></td>
+                <td><?= htmlspecialchars($post['points'])?></td>
+            </tr>
+        <?php endforeach; ?>
     </table>
 </main>
 
