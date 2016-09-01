08848f7fcb720292f7af80194661319ed702865d
Martin <mrindzhov@gmail.com>
2016-09-01 09:29:52 +0300

    Update about.php

--- FDO_Blog/views/home/about.php
+++ FDO_Blog/views/home/about.php
@@ -10,23 +10,26 @@
 
 <p>This is a place where you can find the latest trends and opportunities when it comes to <strong>making money online</strong>. The authors and users of the blog have put relentless effort to bring you closer to your dreams and financial goals. So please, don't miss the opportunity to participate in this community and reap the benefits! </p>
 <h3>Here you can find:</h3>
-<ul>
-    <li>
+<ul class="list-group col-lg-4">
+    <li class="list-group-item">
         Ecommerce advice
     </li>
-    <li>
+    <li class="list-group-item">
         Social media marketing tips
-    </li>
-    <li>
+    </li class="list-group-item">
+    <li class="list-group-item">
         Best practices of copywriting
     </li>
-    <li>
+    <li class="list-group-item">
         SEO tips
     </li>
-    <li>
+    <li class="list-group-item">
         How to find AND create opportunities for yourself as a freelancer
     </li>
 </ul>
-<p>.....and much more!</p>
+<br>
+<div class="container">
+    <p class="col-lg-12">.....and much more!</p>
+</div>
 </body>
 </html>
