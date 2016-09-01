dd2a64f854f7056413b91fd6af868ff41b332215
Martin <mrindzhov@gmail.com>
2016-09-01 09:44:00 +0300

    Update pass.php

--- FDO_Blog/views/users/pass.php
+++ FDO_Blog/views/users/pass.php
@@ -1,17 +1,40 @@
 <?php $this->title = 'Change password'; ?>
 
-<h1><?= htmlspecialchars($this->title) ?></h1>
+<legend><h1><?= htmlspecialchars($this->title) ?></h1></legend>
 
-<form method="post">
-    <div>Enter old Password:</div>
-    <div><input type="password" name="old_password"></div>
+<div class="container">
+    <form method="post" class="form-horizontal">
 
-    <div>Enter new Password:</div>
-    <div><input type="password" name="new_password"></div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="email">Enter old password:</label>
+            <div class="col-sm-4">
+                <input type="password" class="form-control" id="pwd" name="old_password"
+                       placeholder="Enter old password" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="email">Enter new password:</label>
+            <div class="col-sm-4">
+                <input type="password" class="form-control" id="pwd" name="new_password"
+                       placeholder="Enter new password" required>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class="control-label col-sm-2" for="email">Confirm new password:</label>
+            <div class="col-sm-4">
+                <input type="password" class="form-control" id="pwd" name="confirm_password"
+                       placeholder="Enter old password" required>
+            </div>
+        </div>
 
-    <div>Confirm new Password:</div>
-    <div><input type="password" name="confirm_password"></div>
 
-    <div><input type="submit" value="Save changes" onsubmit="<?= $this->changePassword() ?>"></div>
+        <div class="form-group">
+            <div class="col-lg-10 col-lg-offset-2">
+                <button class="btn btn-primary" onsubmit="<?= $this->changePassword() ?>">Save changes</button>
+                <button type="reset" class="btn btn-default"><a href="<?=APP_ROOT?>/users/profile">Cancel</a></button>
 
-</form>
+            </div>
+        </div>
+
+    </form>
+</div>
